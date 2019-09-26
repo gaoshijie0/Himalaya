04be222fb2aafd68922da7acc16387dc3b1e303a
gaoshijie0 <359077754@qq.com>
2019-09-26 23:19:30 +0800

    引入Indicator框架

--- app/src/main/java/com/glandroid/himalaya/MainActivity.java
+++ app/src/main/java/com/glandroid/himalaya/MainActivity.java
@@ -1,49 +1,44 @@
 package com.glandroid.himalaya;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
-import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
-import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
-import com.ximalaya.ting.android.opensdk.model.category.Category;
-import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+import com.glandroid.himalaya.adapters.IndicatorAdapter;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG ="MainActivity" ;
+    private MagicIndicator mMagicIndicator;
+    private ViewPager mContentPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);        
+        iniview();
+
+    }
+
+    private void iniview() {
+        mMagicIndicator = findViewById(R.id.main_indicator);
+        mMagicIndicator.setBackgroundColor(getResources().getColor(R.color.main_color));
+        //创建Indicator适配器
+        IndicatorAdapter adapter = new IndicatorAdapter(this);
+        CommonNavigator commonNavigator = new CommonNavigator(this
+        );
+        commonNavigator.setAdapter(adapter);
+        //Viewpager
+        mContentPager = findViewById(R.id.content_pager);
+        mMagicIndicator.setNavigator(commonNavigator);
+        //把Indicator和viewpager绑定在一起
+        ViewPagerHelper.bind(mMagicIndicator,mContentPager);
 
 
-        Map<String, String> map = new HashMap<>();
-        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
-            @Override
-            public void onSuccess(@Nullable CategoryList categoryList) {
-                List<Category> categories = categoryList.getCategories();
-                if (categories != null) {
-                    int size = categories.size();
-                    Log.d(TAG,"categories size ---<"+ size);
-                    for (Category category : categories) {
-                        Log.d(TAG,"category ---->"+ category.getCategoryName());
-                        
-                    }
-                }
-            }
 
-            @Override
-            public void onError(int i, String s) {
-                Log.e(TAG,"error code"+ i + "error msg" + s);
-
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/glandroid/himalaya/adapters/IndicatorAdapter.java
@@ -0,0 +1,62 @@
+package com.glandroid.himalaya.adapters;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+
+import com.glandroid.himalaya.R;
+
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.indicators.LinePagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.ColorTransitionPagerTitleView;
+
+/**
+ * @author Administrator
+ * @version $Rev$
+ * @dex ${TODO}
+ * @updateAuthor $Author$
+ * @updateDes ${TODO}
+ */
+public class IndicatorAdapter extends CommonNavigatorAdapter {
+
+    private final String[] mTitles;
+
+    public IndicatorAdapter(Context context) {
+        mTitles = context.getResources().getStringArray(R.array.indicator_title);
+    }
+
+    @Override
+    public int getCount() {
+        if (mTitles != null) {
+            return mTitles.length;
+        }
+        return 0;
+    }
+
+    @Override
+    public IPagerTitleView getTitleView(Context context, int index) {
+        ColorTransitionPagerTitleView colorTransitionPagerTitleView = new ColorTransitionPagerTitleView(context);
+        colorTransitionPagerTitleView.setNormalColor(Color.parseColor("#aaffffff"));
+        colorTransitionPagerTitleView.setSelectedColor(Color.parseColor("#ffffff"));
+        colorTransitionPagerTitleView.setText(mTitles[index]);
+        colorTransitionPagerTitleView.setTextSize(18);
+        colorTransitionPagerTitleView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO
+
+            }
+        });
+        return colorTransitionPagerTitleView;
+    }
+
+    @Override
+    public IPagerIndicator getIndicator(Context context) {
+        LinePagerIndicator indicator = new LinePagerIndicator(context);
+        indicator.setMode(LinePagerIndicator.MODE_WRAP_CONTENT);
+        indicator.setColors(Color.WHITE);
+        return indicator;
+    }
+}
